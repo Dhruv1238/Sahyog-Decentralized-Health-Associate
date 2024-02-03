fa7abaa559453834e1515dffbcf5a4c09ecb693b
Pratham Goenka <prathamg1803@gmail.com>
2024-02-03 18:41:36 +0530

    Router specialist

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -10,6 +10,7 @@ import BasicInfo from './pages/BasicInfo';
 import Medical from './pages/Medical';
 import HealthInsur from './pages/HealthInsur';
 import Landing from './pages/landing/Landing';
+import Specialist from './pages/landing/specialist/Specialist';
 
 export const Router = () => {
     return (
@@ -26,6 +27,7 @@ export const Router = () => {
                 <Route path='/medicaldetails' element={<Medical />} />
                 <Route path='/healthinsurance' element={<HealthInsur />} />
                 <Route path='/landing' element={<Landing />} />
+                <Route path="/specialist/:name/:degree" element={<Specialist />} />
             </Routes>
         </>
     );
